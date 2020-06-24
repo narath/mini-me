5f4c4ad24e74aa2bc6dc2a385d7f1cfe9285521d
Narath Carlile <narathc@gmail.com>
2020-06-24 15:24:15 -0400

    Using sprints instead of solids

--- app/main.rb
+++ app/main.rb
@@ -3,36 +3,10 @@
 WIDTH = 1280
 HEIGHT = 720
 
-class Color
-    attr_accessor :r, :g, :b
-    
-    def initialize(r = 0, g = 0, b = 0)
-        @r = r
-        @g = g
-        @b = b
-    end
-    
-    def self.Red
-        Color.new(255,0,0)
-    end
-
-    def self.Orange
-      Color.new(255,127,80)
-    end
-    
-    def self.Green
-        Color.new(0,255,0)
-    end
-
-    def self.Black
-      Color.new(0,0,0)
-    end
-    
-end
-
 class Person
     attr_accessor :x, :y, :infected, :change_x, :change_y
     attr_reader :show_infected_for
+    attr_sprite
     
     PREVALENCE = 7
     SIZE = 25
@@ -44,6 +18,7 @@ class Person
         @infected = infected || rand(100) < PREVALENCE
         @change_x = change_x || rand(10) - 5
         @change_y = change_y || rand(10) - 5
+        self.path = 'sprites/person.png'
     end
 
     def serialize
@@ -57,30 +32,17 @@ class Person
     def to_s
       serialize.to_s
     end
-    
-    def color
-        if @infected
-            Color.Red
-        else
-            Color.Green
-        end
-    end
-    
-    def shape(grow=0, new_color=nil)
-      c = new_color || color
-      [@x, @y, SIZE+grow, SIZE+grow, c.r, c.g, c.b]
-    end
-
-    def border
-      [@x, @y, SIZE, SIZE, Color.Black.r, Color.Black.g, Color.Black.b]
-    end
 
     def show(outputs)
       if @show_infected_for > 0
-        outputs.solids << shape(5, Color.Orange)
+        outputs.sprites << [@x, @y, SIZE, SIZE, "sprites/person-exposed.png"]
         @show_infected_for = @show_infected_for - 1
       else
-        outputs.solids << shape
+        if @infected
+          outputs.sprites << [@x, @y, SIZE, SIZE, "sprites/person-infected.png"]
+        else
+          outputs.sprites << [@x, @y, SIZE, SIZE, "sprites/person.png"]
+        end
       end
     end
 
@@ -113,7 +75,7 @@ class Person
     def check(people)
       return if @infected
 
-      if people.any? { |other| other.rect.intersect_rect?(self.rect) and other.infected }
+      if people.any? { |other| other.infected and other.rect.intersect_rect?(self.rect) }
         @show_infected_for = SHOW_INFECTED_FOR
         @infected = true
       end
